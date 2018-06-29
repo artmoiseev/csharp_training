5b9209f5aaf869c19388f8fc27b6f51fc5c1fbe2
artmoiseev <artur.moiseev1@gmail.com>
2018-06-29 23:12:35 +0300

    refactored test so that it contains readable methods, applied value object pattern

---
+++ addressbook-web-tests/addressbook-web-tests/GroupData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressBookTests
+{
+    class GroupData
+    {
+        private string groupName;
+        private string groupHeader;
+        private string groupFooter;
+
+        public GroupData(string groupName, string groupHeader, string groupFooter)
+        {
+            this.GroupName = groupName;
+            this.GroupHeader = groupHeader;
+            this.GroupFooter = groupFooter;
+        }
+
+        public string GroupName
+        {
+            get { return groupName; }
+            set { groupName = value; }
+        }
+
+        public string GroupHeader
+        {
+            get { return groupHeader; }
+            set { groupHeader = value; }
+        }
+
+        public string GroupFooter
+        {
+            get { return groupFooter; }
+            set { groupFooter = value; }
+        }
+    }
+}
--- addressbook-web-tests/addressbook-web-tests/WebAddressBookTests.cs
+++ addressbook-web-tests/addressbook-web-tests/WebAddressBookTests.cs
@@ -2,15 +2,16 @@
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using addressbook_web_tests;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
-namespace SeleniumTests
+namespace WebAddressBookTests
 {
     [TestFixture]
-    public class Untitled
+    public class WebAddressBookTests
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
@@ -43,26 +44,62 @@ namespace SeleniumTests
         }
 
         [Test]
-        public void TheUntitledTest()
+        public void GroupCreationTest()
+        {
+            //Open home page
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToGroupsPage();
+            InitGroupCreation();
+            FillGroupForm(new GroupData("df", "dg", "fddg"));
+            SubmitGroupCreation();
+            ReturnToGroupsPage();
+        }
+
+        private void ReturnToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("group page")).Click();
+        }
+
+        private void SubmitGroupCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void FillGroupForm(GroupData groupData)
+        {
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(groupData.GroupName);
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(groupData.GroupHeader);
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(groupData.GroupFooter);
+        }
+
+        private void InitGroupCreation()
+        {
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void GoToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        private void Login(AccountData account)
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+        }
+
+        private void OpenHomePage()
         {
             driver.Navigate().GoToUrl(baseURL + "addressbook/");
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
-            driver.FindElement(By.LinkText("groups")).Click();
-            driver.FindElement(By.Name("new")).Click();
-            driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys("fdgdg");
-            driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("gf");
-            driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("fgd");
-            driver.FindElement(By.Name("submit")).Click();
-            driver.FindElement(By.LinkText("group page")).Click();
-            driver.FindElement(By.LinkText("Logout")).Click();
         }
+
         private bool IsElementPresent(By by)
         {
             try
